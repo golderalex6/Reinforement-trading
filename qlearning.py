b0241939fc8f2bc25aff8f15c3326dd30f86089d
golderalex6 <golderalex6@gmail.com>
2024-12-29 03:10:51 +0700

    modified env attributes,logic,..

---
+++ qlearning.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from trading_environment import TradingEnv
+from time import sleep
+
+seed = np.linspace(0,4*np.pi,10)
+# noise = 0.2*np.random.randn(1000)
+
+m = pd.DataFrame(4*np.sin(0.5*seed)+10,columns = ['Close'])
+m['Diff'] = m['Close'].diff(1).fillna(0)
+m['Diff_pct'] = m['Close'].pct_change().fillna(0)*100
+
+env = TradingEnv(df = m,target = 229.99)
+# observation_max = np.array([m['Close'].max(),m['Diff_pct'].max(),1,500])
+# observation_min = np.array([m['Close'].min(),m['Diff_pct'].min(),0,0])
+
+c_learning_rate = 0.1
+c_discount_value = 1
+
+v_epsilon = 0.99
+v_epsilon_decay = 0.0005
+
+q_table_size = [7,20,2,50]
+q_table_segment_size = (env.observation_max - env.observation_min) / q_table_size
+
+def convert_state(real_state):
+    q_state = (real_state - env.observation_min) // (q_table_segment_size*1.00001)
+    return tuple(q_state.astype(int))
+
+q_table = np.random.uniform(low=-2, high=-1, size=(q_table_size + [env.action_space.n]))
+
+
+max_total_portforlio = -(10**9)
+max_ep_reward = -999999999
+max_ep_action_list = []
+
+fg=plt.figure()
+ax=fg.add_subplot()
+
+draw = False
+
+for ep in range(15000):
+
+    next_real_state,reward,terminate,truncate,_=env.reset()
+    current_state=convert_state(next_real_state)
+
+    ep_reward = 0
+    action_list = []
+
+    while True:
+
+        action = np.argmax(q_table[current_state])
+
+        next_real_state, reward, terminate,truncated, _  = env.step(action=action)
+
+        action_list.append(action)
+        ep_reward += reward
+
+        next_state = convert_state(next_real_state)
+        current_q_value = q_table[current_state + (action,)]
+        new_q_value = (1 - c_learning_rate) * current_q_value + c_learning_rate * (reward + c_discount_value * np.max(q_table[next_state]))
+        q_table[current_state + (action,)] = new_q_value
+        current_state = next_state
+
+        if terminate:
+            if env.goal:
+                print(ep,'*'*50)
+            if ep_reward > max_ep_reward:
+                print(ep,'-'*50)
+                max_ep_reward = ep_reward
+                max_ep_action_list = action_list
+
+            max_total_portforlio = max(max_total_portforlio,env.total)
+            break
+
+    if draw:
+        ax.cla()
+        m['Close'].plot(ax=ax)
+        for i in range(len(action_list)):
+            if action_list[i]==0:
+                ax.text(i,m.iloc[i,0],'B',color='C2')
+            if action_list[i]==2:
+                ax.text(i,m.iloc[i,0],'S',color='C3')
+        ax.set_title(f'Episode : {ep}') 
+        plt.pause(0.0001)
+        if len(plt.get_fignums()) == 0:
+            raise
+
+    v_epsilon -= v_epsilon_decay
+
+
+fg.clf()
+ax_1 = fg.add_subplot(1,2,1)
+ax_2 = fg.add_subplot(1,2,2)
+
+m['Close'].plot(ax=ax_1)
+for i in range(len(max_ep_action_list)):
+    if max_ep_action_list[i]==0:
+        ax_1.text(i,m.iloc[i,0],'B',color='C2')
+    if max_ep_action_list[i]==2:
+        ax_1.text(i,m.iloc[i,0],'S',color='C3')
+ax_1.set_title(f'Best actions {max_total_portforlio},{max_ep_reward}')
+
+
+m['Close'].plot(ax=ax_2)
+next_real_state,reward,terminate,truncate,_=env.reset()
+current_state=convert_state(next_real_state)
+i = 0
+while True:
+    action = np.argmax(q_table[current_state])
+    if action == 0:
+        ax_2.text(i,m.iloc[i,0],'B',color='C2')
+    if action == 2:
+        ax_2.text(i,m.iloc[i,0],'S',color='C3')
+    next_real_state, reward, terminate,truncated, _  = env.step(action=action)
+    next_state = convert_state(next_real_state)
+    current_state = next_state
+    i+= 1
+    if terminate:
+        break
+ax_2.set_title(f"Final model's actions {env.total}")
+
+plt.tight_layout()
+plt.show()
--- trading_environment.py
+++ trading_environment.py
@@ -8,14 +8,13 @@ import gymnasium as gym
 plt.rc('figure',titleweight='bold',titlesize='large',figsize=(15,6))
 plt.rc('axes',labelweight='bold',labelsize='large',titleweight='bold',titlesize='large',grid=True)
 
-
 class TradingEnv(gym.Env):
-    def __init__(self,df:pd.DataFrame,window_size=1,render = True) -> None:
+    def __init__(self,df:pd.DataFrame,target:float,initial_balance:float = 100,render:bool = True) -> None:
         """
         Initializes a custom financial environment for reinforcement learning.
 
         This function sets up the environment with financial data, defines the observation 
-        and action spaces, and initializes key attributes such as the balance, coin holdings, 
+        and action spaces, and initializes key attributes such as the balance, _coin _holdings, 
         and price tracking.
 
         Parameters:
@@ -32,38 +31,33 @@ class TradingEnv(gym.Env):
         """
         super().__init__()
 
-        self.df=df.copy()
-        self.df['Diff'] = self.df['Close'].diff(1).fillna(0)
-        self.df['Diff_pct']=self.df['Close'].pct_change(1).fillna(0)*100
-        self.processed_df=self.df[['Close','Diff_pct']]
+        self._df=df.copy()
+        self._close = self._df['Close']
+        self._percentage_change = self._df['Close'].pct_change(1).fillna(0)*100
 
-        self._window_size=window_size
-        self._index=self._window_size-1
+        self.observation_max = np.array([self._close.max(),self._percentage_change.max(),1,500])
+        self.observation_min = np.array([self._close.min(),self._percentage_change.min(),0,0])
+
+        self._initial_balance = initial_balance
+        self._target = target
         
-        self.observation_space=gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self._window_size,self.processed_df.shape[1]),dtype=float)
+        # self.observation_space=gym.spaces.Box(low=-np.inf, high=np.inf, shape=(4,),dtype=float)
+        self.observation_space=gym.spaces.Box(
+                            low = np.array([self._close.min(),self._percentage_change.min(),0,0.0]),
+                            high = np.array([self._close.max(),self._percentage_change.max(),1,500.0]),
+                            shape=(4,),
+                            dtype=float
+                        )
+
         self.action_space=gym.spaces.Discrete(3)
 
-        self.observation_max=self.processed_df.max().values
-        self.observation_min=self.processed_df.min().values
-
-        self.initial_balance = 100
-        self.usd = self.initial_balance
-        self.coin = 0
-
-        self.buy_price = self.df.iloc[self.index,0]
-        self.sell_price = self.df['Close'].max()
-
-        self.hold = 0
-
-        self.goal = False
-        
     def step(self,action:int) -> typing.Iterable:
         """
         Performs a single step in the financial trading environment based on the given action.
 
         This method updates the environment's state, calculates the reward, and determines 
         whether the episode should terminate or truncate. It handles the buying, selling, 
-        or holding of assets and adjusts the balance and coin holdings accordingly.
+        or _holding of assets and adjusts the balance and _coin holdings accordingly.
 
         Parameters:
         -----------
@@ -86,40 +80,39 @@ class TradingEnv(gym.Env):
         reward = -1
 
         if action==2:
-            if self.coin>0:
+            if self._coin>0:
 
-                self.sell_price = self.df.iloc[self.index,0]
-                self.buy_price = None
+                self._sell_price = self._close.iloc[self._index]
+                self._buy_price = None
                 
-                self.usd = self.df.iloc[self.index,0]*self.coin
-                self.coin = 0
+                self._usd = self._close.iloc[self._index]*self._coin
+                self._coin = 0
 
-                self.hold = 0
+                self._hold = 0
             else:
                 reward  = -2
 
         elif action==0:
-            if self.usd>0:
+            if self._usd>0:
 
-                self.buy_price = self.df.iloc[self.index,0]
-                self.sell_price = None
+                self._buy_price = self._close.iloc[self._index]
+                self._sell_price = None
 
-                self.coin = self.usd/self.df.iloc[self.index,0]
-                self.usd = 0
+                self._coin = self._usd/self._close.iloc[self._index]
+                self._usd = 0
 
-                self.hold = 1
+                self._hold = 1
             else:
                 reward  = -2
 
-        self.index+=1
-        self.total = self.df.iloc[self.index,0]*self.coin + self.usd
-        new_state=self.processed_df.iloc[self.index].values.tolist()
-        new_state.extend([self.hold,self.total])
+        self._index+=1
+        self.total = self._close.iloc[self._index]*self._coin + self._usd
+        new_state = [self._close.iloc[self._index],self._percentage_change.iloc[self._index],self._hold,self.total]
 
-        if self.index==self.df.shape[0]-1:
-            if self.total>= 200:
+        if self._index==self._df.shape[0]-1:
+            if self.total>= self._target:
                 self.goal = True
-                reward = 10000
+                reward = 1000
             terminate,truncate=True,True
 
         return new_state,reward,terminate,truncate,{}
@@ -130,7 +123,7 @@ class TradingEnv(gym.Env):
         Resets the environment to its initial state.
 
         This method initializes or resets all key attributes of the environment, such as 
-        balance, coin holdings, and indices, to prepare for a new episode.
+        balance, _coin _holdings, and indices, to prepare for a new episode.
 
         Parameters:
         -----------
@@ -145,21 +138,19 @@ class TradingEnv(gym.Env):
                 - info (dict): Additional information (empty dictionary in this case).
         """
 
-        self.index=self.window_size-1
-        self.usd = self.initial_balance
-        self.coin = 0
-        self.total = self.df.iloc[self.index,0]*self.coin + self.usd
+        self._index=0
 
-        self.buy_price = self.df.iloc[self.index,0]
-        self.sell_price = self.df['Close'].max()
+        self._usd = self._initial_balance
+        self._coin = 0
+        self.total = self._close.iloc[self._index]*self._coin + self._usd
 
-        self.hold = 0
+        self._buy_price = None
+        self._sell_price = None
 
-        new_state = self.processed_df.iloc[self.index].values.tolist()
-        new_state.append(self.hold)
-        new_state.append(self.total)
-
+        self._hold = 0
         self.goal = False
+
+        new_state = [self._close.iloc[self._index],self._percentage_change.iloc[self._index],self._hold,self.total]
 
         return new_state,None,False,False,{}
 
@@ -170,4 +161,12 @@ class TradingEnv(gym.Env):
         pass
 
 if __name__=='__main__':
-    pass
+    seed = np.linspace(0,4*np.pi,10)
+# noise = 0.2*np.random.randn(1000)
+
+    m = pd.DataFrame(4*np.sin(0.5*seed)+10,columns = ['Close'])
+    m['Diff'] = m['Close'].diff(1).fillna(0)
+    m['Diff_pct'] = m['Close'].pct_change().fillna(0)*100
+
+    env = TradingEnv(df = m,target = 229.99)
+    print(env.observation_space.high)
